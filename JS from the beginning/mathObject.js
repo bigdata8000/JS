182d53219a9873ea63bd676b0ab48ed78230d940
Henry Phu-Henh Lam <50170584+henrymmmm@users.noreply.github.com>
2019-09-08 15:31:43 +0100

    Add files via upload

---
+++ JS from the beginning/mathObject.js
@@ -0,0 +1,51 @@
+const num1 = 100;
+const num2 = 50;
+let val;
+
+// simple math with numbers
+val_1 = num1 + num2;
+val_2 = num1 * num2;
+val_3 = num1 - num2;
+val_4 = num1 / num2;
+val_5 = num1 % num2;  //  modulus
+
+console.log(val_1);
+console.log(val_2);
+console.log(val_3);
+console.log(val_4);
+console.log(val_5);
+
+
+
+/* ---------------------------------------- */
+
+// math objec
+val_11 = Math.PI;
+val_12 = Math.E;
+val_13 = Math.round(2.4);
+val_14 = Math.ceil(2.4);
+val_15 = Math.floor(2.8);
+val_16 = Math.sqrt(81);
+val_16 = Math.abs(-1000);
+val_17 = Math.pow(7, 2);
+val_18 = Math.min(2,33,4,1,55,6,3,8, -2);
+val_19 = Math.max(2,33,4,1,55,6,3,8, -2);
+val_20 = Math.floor(Math.random() * 20 + 1);    // random number between 1 - 20.
+
+
+console.log(val_11);
+console.log(val_12);
+console.log(val_13);
+console.log(val_14);
+console.log(val_15);
+console.log(val_16);
+console.log(val_17);
+console.log(val_18);
+console.log(val_19);
+console.log(val_20);
+
+
+/* ---------------------------------------- */
+
+
+
